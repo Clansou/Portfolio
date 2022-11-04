68d44c748e18993038fc1640d4d3e20202e3f744
Clansou <clement.souplet78@gmail.com>
2022-11-04 11:58:48 +0100

    :Zap: Update Link

--- script.js
+++ script.js
@@ -19,4 +19,21 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.message.value = '';
   });
 
+let anchorSelector = 'a[href^="#"]';
+let anchorList = document.querySelectorAll(anchorSelector);
+anchorList.forEach(link => {
+    link.onclick = function (scroll) {
+
+        scroll.preventDefault();
+
+        let destination =
+            document.querySelector(this.hash);
+        
+ 
+        destination.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+});
+
 
